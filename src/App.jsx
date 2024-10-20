067364af7e3cbee0664757c3df6ff575fe06cfc0
sumaiya-rahman-chowdhury <bdfara@1>
2024-10-19 21:03:07 -0700

    Rendering the list of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 
+const users = [
+  { firstName: "John", id: 1 },
+  { firstName: "Emily", id: 2 },
+  { firstName: "Michael", id: 3 },
+  { firstName: "Sarah", id: 4 },
+  { firstName: "David", id: 5 },
+  { firstName: "Jessica", id: 6 },
+  { firstName: "Daniel", id: 7 },
+  { firstName: "Olivia", id: 8 },
+  { firstName: "Matthew", id: 9 },
+  { firstName: "Sophia", id: 10 }
+]
+
 function App() {
 
   const [searchItem,setSearchItem] = useState('')
@@ -15,6 +28,11 @@ function App() {
     onChange={handleInputChange}
     placeholder='Type to search'
     />
+    <ul>
+      {
+        users.map(user=><li key={user.id}>{user.firstName}</li>)
+      }
+    </ul>
     </>
   )
 }
